e16ae0bf245cc03147c71ab2ef09d9a3f9d1ee6e
Thanh Hoang <thanh.hoang@sapotacorp.vn>
2022-05-05 16:40:11 +0700

    Update

---
+++ BaiTap1/Jquery/Edit_SV.js
@@ -0,0 +1,73 @@
+﻿$(document).ready(function () {
+    console.log(call_class - 1);
+    //bỏ tr đang click lên popup
+    for (var k in json) {
+        if (json[k].Ma == call_class) {
+            $('#date').val(json[k].NgaySinh);
+            $('#date').val(json[k].NgaySinh);
+            $('#HoTen').val(json[k].HoTen);
+            $('#Ma').val(json[k].Ma);
+
+            if (json[k].GioiTinh == "Nam") {
+                var $radios = $('input:radio[name=flexRadioDefault]');
+                $radios.filter('[value=Male]').prop('checked', true);
+            }
+            else {
+                var $radios = $('input:radio[name=flexRadioDefault]');
+                $radios.filter('[value=Female]').prop('checked', true);
+            }
+            if (json[k].DaLayBang == "true") {
+                $("#DaLayBang").prop("checked", true);
+            }
+            else {
+                $("#DaLayBang").prop("checked", false);
+            }
+            $(".content").html(json[k].Lop);
+            $('#khoa').val(json[k].Khoa);
+        }
+    }
+    $('#edit_sv').click(function () {
+        if ($("#HoTen").val() == "" || $("#Ma").val() == "" || $("#Khoa").val() == "") {
+            alert("Vui lòng nhập đủ thông tin !!!");
+        }
+        var DaLayBang;
+        var GioiTinh;
+        if ($("#DaLayBang").prop("checked") == true) {
+            DaLayBang = "true";
+        }
+        else if ($("#DaLayBang").prop("checked") == false) {
+            DaLayBang = "false";
+        }
+        GioiTinh = $("input[name='flexRadioDefault']:checked").val();
+        if (GioiTinh == "Male") {
+            GioiTinh = "Nam";
+        }
+        if (GioiTinh == "Female") {
+            GioiTinh = "Nữ";
+
+        }
+
+        for (var k in json) {
+            if (json[k].Ma == call_class) {
+                //Không update mã dù người dùng sửa mã:Mã ko cho sửa
+                json[k].HoTen = $("#HoTen").val();
+                json[k].NgaySinh = $("#date").val();
+                json[k].GioiTinh = GioiTinh;
+                json[k].DaLayBang = DaLayBang;
+                json[k].DiaChi = $('#form-select').val();
+                json[k].Lop = $(".content").html();
+                json[k].Khoa = $("#khoa").val();
+            }
+        }
+        $("tbody tr").remove();
+        Load();
+    });
+});
+$("#reset").click(function () {
+    $('#date').val("");
+    $('#HoTen').val("");
+    $('#Ma').val("");
+    $(".content").html("");
+    $('#khoa').val("");
+    alert("Đã đặt lại !");
+});
--- BaiTap1/Jquery/JavaScript.js
+++ BaiTap1/Jquery/JavaScript.js
@@ -1,8 +1,11 @@
 ﻿var call_class;
 //lấy ra class của td đang click, call ra để dùng biến này bỏ tr lên popup khi sửa
 $(document).ready(function () {
+    $("td").css('color', 'red');
     $('tr:even').css('background-color', 'white');
     $("td:nth-child(5)").css('text-align', 'center');
+    $("td:nth-child(1)").css('text-align', 'right');
+    $("td:nth-child(2),td:nth-child(6),td:nth-child(7),td:nth-child(8)").css('text-align', 'left');
     $('.buttonThem').click(function () {
         $('.popup-content').load('Form.html');
         Add();
@@ -24,43 +27,48 @@ $(document).ready(function () {
     //    paginate.init('.myTable', options, filterOptions);
     //}
     //PhanTrang();
-    //Xóa
-    Delete();
     //sửa
     Edit();
     //Thêm
     Add();
     //Tìm theo địa chỉ
-    $("#find_diachi").keypress(function () {
-        $("tbody tr").remove();
-        for (var k in json) {
-            if (json[k].DiaChi == $(this).val()) {
-                let check = (json[k].DaLayBang == "true") && "checked";
-                $("#add").append(
-                    "<tr>" +
-                    "<td>" + json[k].Ma + "</td>" +
-                    "<td>" + json[k].NgaySinh + "</td>" +
-                    "<td>" + json[k].GioiTinh + "</td>" +
-                    "<td>" + "<input " + check + " type='checkbox' name='checkbox' value= '" + json[k].DaLayBang + "' />    </td>" +
-                    "<td>" + json[k].DiaChi + "</td>" +
-                    "<td>" + json[k].Lop + "</td>" +
-                    "<td>" + json[k].Khoa + "</td>" +
-                    "<td" + " class='" + json[k].Ma + "'>" + "<img src = '../images/dauX.PNG' width='10px' height='10px'/> " + "</td>" +
-                    "<td" + " class='" + json[k].Ma + "'>" + "<img src = '../images/folder.PNG' width='10px' height='10px'/>" + "</td>"
-                    + "</tr>"
-                );
+    $('#find_diachi').keypress(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            $("tbody tr").remove();
+            for (var k in json) {
+                if (json[k].DiaChi == $(this).val()) {
+                    let check = (json[k].DaLayBang == "true") && "checked";
+                    $("#add").append(
+                        "<tr>" +
+                        "<td>" + json[k].Ma + "</td>" +
+                        "<td>" + json[k].HoTen + "</td>" +
+                        "<td>" + json[k].NgaySinh + "</td>" +
+                        "<td>" + json[k].GioiTinh + "</td>" +
+                        "<td>" + "<input " + check + " type='checkbox' name='checkbox' value= '" + json[k].DaLayBang + "' />    </td>" +
+                        "<td>" + json[k].DiaChi + "</td>" +
+                        "<td>" + json[k].Lop + "</td>" +
+                        "<td>" + json[k].Khoa + "</td>" +
+                        "<td" + " class='" + json[k].Ma + "'>" + "<img src = '../images/dauX.PNG' width='7px' height='7px'/> " + "</td>" +
+                        "<td" + " class='" + json[k].Ma + "'>" + "<img src = '../images/sua.PNG' width='7px' height='7px'/>" + "</td>"
+                        + "</tr>"
+                    );
+                }
             }
+            if ($(this).val() == "") {
+                Load();
+            }
+            Delete();
+            Edit();
+            $("td:nth-child(1)").css('text-align', 'right');
+            $("td:nth-child(2),td:nth-child(6),td:nth-child(7),td:nth-child(8)").css('text-align', 'left');
+            $("td:nth-child(2)").css('color', 'blue');
+            $('tr:even').css('background-color', '#CCE5FF');
         }
-        if ($(this).val() == "") {
-            Load();
-        }
-        Delete();
-        Edit();
-        $("td:nth-child(2)").css('color', 'blue');
-        $('tr:even').css('background-color', '#CCE5FF');
     });
 });
 function Load() {
+   
     $("tbody tr").remove();
     for (var k in json) {
         let check = (json[k].DaLayBang == "true") && "checked";
@@ -75,12 +83,16 @@ function Load() {
             "<td>" + json[k].Lop + "</td>" +
             "<td>" + json[k].Khoa + "</td>" +
             "<td" + " class='" + json[k].Ma + "'>" + "<img src = '../images/dauX.PNG' width='10px' height='10px'/>  " + "</td>" +
-            "<td" + " class='" + json[k].Ma + "'>" + "<a href='#xmas-popup'>" + "<img src = '../images/folder.PNG' width='10px' height='10px'/>" + "</a>" + "</td>"
+            "<td" + " class='" + json[k].Ma + "'>" + "<a href='#xmas-popup'>" + "<img src = '../images/sua.PNG' width='50px' height='50px'/>" + "</a>" + "</td>"
             + "</tr>"
         );
     }
+    $('#IdCheckBox').attr('readonly', true);
+    $("td:nth-child(1)").css('text-align', 'right');
+    $("td:nth-child(2),td:nth-child(6),td:nth-child(7),td:nth-child(8)").css('text-align', 'left');
     $('tr:even').css('background-color', '#CCE5FF');
     $("td:nth-child(2)").css('color', 'blue');
+    $("td:nth-child(1)").css('color', 'blue');
     //PhanTrang();
     //Add();
     Edit();
@@ -91,9 +103,13 @@ function Add() {
     $('#add_sv').click(function () {
         //check xem người dùng nhập mã có trùng vs mã cũ ko
         var checkMaLap = true;
+        if ($("#Ma").val() == "" || $("#HoTen").val() == "") {
+            alert("Vui lòng nhập đủ thông tin cần thiết ?")
+        }
         for (var k in json) {
             if (json[k].Ma == $("#Ma").val() || $("#date").val() == "" || $("#khoa").val() == "") {
                 checkMaLap = false;
+                alert("Mã bị trùng lặp")
                 break;
             }
         }
@@ -117,10 +133,13 @@ function Add() {
             // console.log(json);
             $('tr:even').css('background-color', '#CCE5FF');
         }
+       
         $("tbody tr").remove();
         Load();
         //Delete();
         Edit();
+        $("td:nth-child(1)").css('text-align', 'right');
+        $("td:nth-child(2),td:nth-child(6),td:nth-child(7),td:nth-child(8)").css('text-align', 'left');
     });
 }
 function Edit() {
@@ -129,14 +148,17 @@ function Edit() {
         let this_class = $(this).attr('class');
         call_class = this_class;
         $('.popup-content').load('Form.html');
-
+        $("td:nth-child(1)").css('text-align', 'right');
+        $("td:nth-child(2),td:nth-child(6),td:nth-child(7),td:nth-child(8)").css('text-align', 'left');
     });
 }
 function Delete() {
     $("td:nth-last-child(2)").click(function () {
         // lấy ra class của cái td đang click
         var this_class = $(this).attr('class');
-        for (var i = 0; i < json.length; i++) {
+        var result = confirm("Bạn có muốn xóa sinh viên có mã " + this_class + " ?");
+        if (result) {
+           for (var i = 0; i < json.length; i++) {
             if (json[i].Ma == this_class) {
                 json.splice(i, 1);
             }
@@ -144,6 +166,10 @@ function Delete() {
         //console.log(json);
         $("tbody tr").remove();
         Load();
+        $("td:nth-child(1)").css('text-align', 'right');
+        $("td:nth-child(2),td:nth-child(6),td:nth-child(7),td:nth-child(8)").css('text-align', 'left');
+        }
+        
     });
 }
 //Lọc theo giới tính
@@ -165,7 +191,7 @@ function Gender() {
                     "<td>" + json[k].Lop + "</td>" +
                     "<td>" + json[k].Khoa + "</td>" +
                     "<td" + " class='" + json[k].Ma + "'>" + "<img src = '../images/dauX.PNG' width='10px' height='10px'/> " + "</td>" +
-                    "<td" + " class='" + json[k].Ma + "'>" + "<a href='#xmas-popup'>" + "<img src = '../images/folder.PNG' width='10px' height='10px'/>" + "</a>" + "</td>"
+                    "<td" + " class='" + json[k].Ma + "'>" + "<a href='#xmas-popup'>" + "<img src = '../images/sua.PNG' width='10px' height='10px'/>" + "</a>" + "</td>"
                     + "</tr>"
                 );
             }
@@ -188,7 +214,7 @@ function Gender() {
                     "<td>" + json[k].Lop + "</td>" +
                     "<td>" + json[k].Khoa + "</td>" +
                     "<td" + " class='" + json[k].Ma + "'>" + "<img src = '../images/dauX.PNG' width='10px' height='10px'/> " + "</td>" +
-                    "<td" + " class='" + json[k].Ma + "'>" + "<a href='#xmas-popup'>" + "<img src = '../images/folder.PNG' width='10px' height='10px'/>" + "</a>" + "</td>"
+                    "<td" + " class='" + json[k].Ma + "'>" + "<a href='#xmas-popup'>" + "<img src = '../images/sua.PNG' width='10px' height='10px'/>" + "</a>" + "</td>"
                     + "</tr>"
                 );
 
@@ -209,7 +235,7 @@ function Gender() {
                 "<td>" + json[k].Lop + "</td>" +
                 "<td>" + json[k].Khoa + "</td>" +
                 "<td" + " class='" + json[k].Ma + "'>" + "<img src = '../images/dauX.PNG' width='10px' height='10px'/> " + "</td>" +
-                "<td" + " class='" + json[k].Ma + "'>" + "<a href='#xmas-popup'>" + "<img src = '../images/folder.PNG' width='10px' height='10px'/>" + "</a>" + "</td>"
+                "<td" + " class='" + json[k].Ma + "'>" + "<a href='#xmas-popup'>" + "<img src = '../images/sua.PNG' width='10px' height='10px'/>" + "</a>" + "</td>"
                 + "</tr>"
             );
         }
@@ -218,5 +244,7 @@ function Gender() {
     Edit();
     $('tr:even').css('background-color', '#CCE5FF');
     $("td:nth-child(2)").css('color', 'blue');
+    $("td:nth-child(1)").css('text-align', 'right');
+    $("td:nth-child(2),td:nth-child(6),td:nth-child(7),td:nth-child(8)").css('text-align', 'left');
 
 }
